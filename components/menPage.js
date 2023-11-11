a7e26834ed4ca6791cbab4d249ace7c28a8b79fe
Peprah Dickson Daniel <techpepson@gmail.com>
2023-11-11 02:49:03 +0000

    Complete the lazy loading property and the slide in of the images of our  work. Work on the page up and the next arrows in next code.

--- components/menPage.js
+++ components/menPage.js
@@ -1,17 +1,29 @@
 //This is the men page effects made with js
+import "../styles/menStyles.css";
+//This is the function component for the lazy loading images.
 function lazyLoading() {
+  //Calling the images and caption classes.
   const imagesToDisplay = document.querySelectorAll(".images");
+  const lazyCaption = document.querySelectorAll(".caption");
+  //Definition of intersection observer
   const observer = new IntersectionObserver((entries, observer) => {
     entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-            console.log(entry.target)
-            observer.unobserve(entry.target);
-        }
+      if (entry.isIntersecting) {
+        entry.target.classList.remove("lazyLoad");
+        setTimeout(() => {
+          entry.target.classList.add("lazyLoad");
+        }, 0);
+          observer.unobserve(entry.target);
+          console.log(entry.target)
+      }
     });
   });
-
+  //Point of observation of the elements
   imagesToDisplay.forEach((images) => {
     observer.observe(images);
+  });
+  lazyCaption.forEach((caption) => {
+    observer.observe(caption);
   });
 }
 
